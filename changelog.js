e643be0cdd988123bb648fad9157f8c1b1201855
Sam Holmes <samholmes1337@gmail.com>
2016-02-12 10:52:42 +0000

    Add basic command line interface

---
+++ changelog.js
@@ -0,0 +1,21 @@
+// Dependencies
+var fs = require("fs");
+
+// Export back to app
+module.exports = {
+
+    // Initialize a new changelog
+    init : function(){
+
+
+
+    },
+
+    // Show the documentation
+    docs : function(){
+
+        console.log("usage: changelog <command> [<args]");
+
+    }
+
+}
---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+// Library functions
+var changelog = require("./changelog");
+
+// Remove first two unneeded CLI args
+process.argv.splice(0, 2);
+
+if (process.argv.length === 0) {
+
+    // No command line arugments specified, show the docs
+    changelog.docs();
+
+} else {
+
+    // Command line arguments specified, go through them
+    switch(process.argv[0]) {
+
+        // Show the help
+        case "help":
+            changelog.docs();
+            break;
+
+        default:
+            // Unrecognised command supplied, show error
+            console.log("changelog: '" + process.argv[0] + "' is not a changelog command. See 'changelog help'.");
+
+    }
+
+}
