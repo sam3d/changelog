239feb9e03373b22abed6d047d09e91b82265cbe
Sam Holmes <samholmes1337@gmail.com>
2016-02-19 22:31:23 +0100

    Create basic API with 'parse' and 'stringify' methods

--- index.js
+++ index.js
@@ -3,70 +3,81 @@
 // Library functions
 var changelog = require("./changelog");
 
-// Remove first two unneeded CLI args
-process.argv.splice(0, 2);
+// If called as a command line app
+if (require.main === module) {
 
-if (process.argv.length === 0) {
+    // Remove first two unneeded CLI args
+    process.argv.splice(0, 2);
 
-    // No command line arugments specified, show the docs
-    changelog.public.docs();
+    if (process.argv.length === 0) {
+
+        // No command line arugments specified, show the docs
+        changelog.public.docs();
+
+    } else {
+
+        // Command line arguments specified, go through them
+        switch(process.argv[0]) {
+
+            // Show the help
+            case "help":
+                changelog.public.docs();
+                break;
+
+            // Initialize a blank changelog file
+            case "init":
+                changelog.public.init();
+                break;
+
+            // Destroy any changelog file
+            case "destroy":
+                changelog.public.destroy();
+                break;
+
+            case "parse":
+                if (process.argv[1]) {
+                    changelog.public.parse(process.argv[1]);
+                } else {
+                    changelog.public.parse();
+                }
+                break;
+
+            // A core changelog function
+            case "add":
+            case "change":
+            case "deprecate":
+            case "remove":
+            case "fix":
+            case "secure":
+                changelog.update(process.argv[0]);
+                break;
+
+            // Bump the version number
+            case "bump":
+                changelog.bump(process.argv[1]);
+                break;
+
+            // Show the changelog status
+            case "status":
+                changelog.public.status();
+                break;
+
+            // Unavailable functionality
+            // case null:
+                // changelog.display("'"  + process.argv[0] + "' is not available yet. Sorry :/");
+
+            default:
+                // Unrecognised command supplied, show error
+                changelog.display("'" + process.argv[0] + "' is not a changelog command. See 'changelog help'.");
+
+        }
+
+    }
 
 } else {
 
-    // Command line arguments specified, go through them
-    switch(process.argv[0]) {
-
-        // Show the help
-        case "help":
-            changelog.public.docs();
-            break;
-
-        // Initialize a blank changelog file
-        case "init":
-            changelog.public.init();
-            break;
-
-        // Destroy any changelog file
-        case "destroy":
-            changelog.public.destroy();
-            break;
-
-        case "parse":
-            if (process.argv[1]) {
-                changelog.public.parse(process.argv[1]);
-            } else {
-                changelog.public.parse();
-            }
-            break;
-
-        // A core changelog function
-        case "add":
-        case "change":
-        case "deprecate":
-        case "remove":
-        case "fix":
-        case "secure":
-            changelog.update(process.argv[0]);
-            break;
-
-        // Bump the version number
-        case "bump":
-            changelog.bump(process.argv[1]);
-            break;
-
-        // Show the changelog status
-        case "status":
-            changelog.public.status();
-            break;
-
-        // Unavailable functionality
-        // case null:
-            // changelog.display("'"  + process.argv[0] + "' is not available yet. Sorry :/");
-
-        default:
-            // Unrecognised command supplied, show error
-            changelog.display("'" + process.argv[0] + "' is not a changelog command. See 'changelog help'.");
-
-    }
+    // Called as a module, export API functions
+    exports.parse = changelog.parse;
+    exports.stringify = changelog.stringify;
 
 }
