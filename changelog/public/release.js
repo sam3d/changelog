e42413cb2057dc217debdf352e0b8fc47935aaef
Sam Holmes <samholmes1337@gmail.com>
2016-03-02 11:45:31 +0000

    Fix error handling and continue "release" command

--- changelog/public/release.js
+++ changelog/public/release.js
@@ -3,24 +3,40 @@ var fs = require("fs");
 var publishRelease = require("publish-release");
 var ghauth = require("ghauth");
 var spinner = require("simple-spinner");
+var exec = require("child_process").exec;
 
 module.exports = function(){
 
     // Read the changelog file
     fs.readFile("CHANGELOG.md", "utf8", function(err, data){
         if (err){
-            changelog.display("fileNotFound");
+            changelog.display(null, "fileNotFound");
         } else {
 
             // Parse the file
             changelog.parse(data, function(err, docs){
                 if (err){
-                    changelog.display("parseError");
+                    changelog.display(null, "parseError");
                 } else {
 
-                    // CHANGELOG read and parsed correctly, attempt
-                    // to read the git remotes and then publish the
-                    // release to GitHub.
+                    // Attempt to get the remotes
+                    exec("git remote -v", function(err, stdout, stderr){
+                        if (err || stderr){
+                            changelog.display("Could not get git remotes", "Fatal");
+                        } else {
+
+                            // Split the remotes
+                            var remotes = stdout.split(/[\s\t]+/);
+
+                            // Look for origin URL
+                            if (remotes.indexOf("origin") != -1){
+                                // Found origin
+                            } else {
+                                // Could not find origin
+                            }
+
+                        }
+                    });
 
                 }
             });
