260b7afb5e23046af48968e105434a63a6cedd37
Sam Holmes <samholmes1337@gmail.com>
2016-03-02 12:37:39 +0000

    Create a new commit and tag and push to github

--- changelog/public/release.js
+++ changelog/public/release.js
@@ -6,6 +6,7 @@ var spinner = require("simple-spinner");
 var exec = require("child_process").exec;
 var isGitUrl = require("is-git-url");
 var parseGithubUrl = require("parse-github-url");
+var fileExists = require("file-exists");
 
 module.exports = function(){
 
@@ -36,6 +37,59 @@ module.exports = function(){
                                 // Make sure it's a git URL
                                 var url = remotes[remotes.indexOf("origin") + 1];
                                 if (isGitUrl(url)){
+
+                                    // Parse the URL
+                                    var remote = parseGithubUrl(url);
+
+                                    // Authenticate the user with github
+                                    ghauth({
+                                        configName: "changelog",
+                                        scopes: ['repo', 'public_repo'],
+                                        note: "Allows the changelog CLI to publish releases",
+                                        userAgent: "changelogapp"
+                                    }, function(err, data){
+
+                                        // If there is an error, notify
+                                        if (err){
+                                            changelog.display("Could not authenticate you with GitHub", "Fatal");
+                                        } else {
+
+                                            // Make sure latest version has been released
+                                            if (docs[0].released){
+
+                                                // Get the version
+                                                var version = docs[0].version;
+
+                                                // Create the command
+                                                var command = "git reset && ";
+                                                command += "git add CHANGELOG.md ";
+                                                command += fileExists("package.json") ? "package.json && " : "&& ";
+                                                command += "git commit -m '" + version + "' && ";
+                                                command += "git tag v" + version + "&& ";
+                                                command += "git push origin master && git push origin v" + version;
+
+                                                // Show the progress spinner
+                                                changelog.display("Creating new commit and tag v" + version + " and pushing to github");
+                                                spinner.start();
+
+                                                // Make the release!
+                                                exec(command, function(err, stdout, stderr){
+
+                                                    // Stop the spinner
+                                                    spinner.stop();
+
+                                                    // Publish the release
+                                                    
+
+                                                });
+
+                                            } else {
+                                                changelog.display("The CHANGELOG.md has unreleased content, could not release", "Fatal");
+                                            }
+
+                                        }
+
+                                    });
 
                                 } else {
 
